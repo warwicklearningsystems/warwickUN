3768b7938db8d67976596af600e823c839ec9ce3
team-learncity <dev@learncity.co.uk>
2022-11-23 20:45:23 -0300

    reintroduce 2 items in the theme footer

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2022110804;
+$plugin->version = 2022112302;
 
 // This is the named version.
 $plugin->release = '3.11.27';
